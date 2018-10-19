4ca421edbf97c089947dd00d8db961980baa0a2e
Chris Armendarez <chris.michael.armo@gmail.com>
2018-10-19 04:39:34 -0700

    Add root, article, scraper & comment routes

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,123 @@
 
 // Dependencies -------------------- //
 
-const router = require("express").Router();
+const axios = require("axios"),
+    cheerio = require("cheerio"),
+    router = require("express").Router();
+const db = require("../models");
 
 // Routes -------------------- //
 
 // Root
 router.get("/", (req, res) => {
-    res.render("index", {
-        title: "Artisan Scraped News",
-        home: "active"
-    });
+    db.Article
+        .find()
+        .count()
+        .then((count) => {
+            res.render("index", {
+                title: "Artisan Scraped News",
+                home: "active",
+                scraped: count > 0
+            });
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 // Articles
 router.get("/articles", (req, res) => {
-    res.render("collection", {
-        title: "Scraped Articles",
-        subtitle: "Saved Articles",
-        articles: "active"
-    });
+    db.Article
+        .find({})
+        .then((scrapedArticles) => {
+            res.status(200).render("collection", {
+                title: "Scraped Articles",
+                subtitle: "Saved Articles",
+                articles: "active",
+                scrapedArticles
+            });
+        })
+        .catch((err) => {
+            res.status(404).json(err);
+        });
 });
 
 // Scrape
-router.get("/scrape", (req, res) => {
-    res.status(200).json({scraped: "success"});
+router.get("/scraper", (req, res) => {
+    axios
+        .get("https://www.huffingtonpost.com/section/green")
+        .then((ax) => {
+            const $ = cheerio.load(ax.data),
+                cards = $(".a-page__column--center .card"),
+                cardsArr = [];
+            cards.each(function(i) {
+                const cardContent = $(this).find(".card__content"),
+                    headlines = cardContent.find(".card__headlines"),
+                    article = {
+                        headline: $(this).find(".card__label__text").text(),
+                        summary: headlines.find(".card__headline__text").text().trim(),
+                        url: `https://www.huffingtonpost.com${cardContent.find(".card__link").attr("href")}`,
+                        src: cardContent.find(".card__image img").attr("src")
+                    };
+                if (article.headline) cardsArr.push(article);
+            });
+            db.Article
+                .insertMany(cardsArr)
+                .then(() => {
+                    res.redirect(303, "/articles");
+                })
+                .catch((err) => {
+                    res.status(500).json(err);
+                });
+        })
+        .catch((err) => {
+            res.status(404).json(err);
+        });
+});
+
+// Get comment
+router.get("/comments/:id", (req, res) => {
+    const {id} = req.params;
+    db.Article
+        .findOne({_id: id})
+        .then((article) => {
+            return db.Comment
+                .find({_id: article.comment});
+        })
+        .then((comments) => {
+            res.status(200).json(comments);
+        })
+        .catch((err) => {
+            res.send(500).json(err);
+        });
+});
+
+// Create comment
+router.post("/comments", (req, res) => {
+    const {id, title, body} = req.body;
+    db.Comment
+        .create({title, body})
+        .then((comment) => {
+            return db.Article.findOneAndUpdate({_id: id}, {comment: comment._id}, {new: true});
+        })
+        .then((article) => {
+            res.status(200).json(article);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+// Clear scraped data
+router.delete("/clear", (req, res) => {
+    db.Article
+        .remove({})
+        .then(() => {
+            res.redirect(303, "/articles");
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 // Export -------------------- //
